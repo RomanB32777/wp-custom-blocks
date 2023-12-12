a10481a0959bac70709618697a2ae4605875017f
Roman Bogdanovskiy <r.bogdanovskiy@onpoint.ru>
2023-12-12 19:47:12 +0300

    fix versions

--- custom-top-blocks.php
+++ custom-top-blocks.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Topbook wp blocks
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           0.1.0
+ * Version:           0.1.1
  * Author:            Test
  * License:
  * License URI:
