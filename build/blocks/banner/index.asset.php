5fcee8209c97c2fccba4342a556461132e75e388
Roman Bogdanovskiy <r.bogdanovskiy@onpoint.ru>
2023-12-24 22:42:13 +0300

    update build

--- build/blocks/banner/index.asset.php
+++ build/blocks/banner/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => 'b2b0573d97cbe9817ab8');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '67ad74f3b27300c4ee33');
