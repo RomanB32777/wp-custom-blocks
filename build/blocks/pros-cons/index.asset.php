b6c2a3c50ae43644c57f4d3f1b383aca985b85b0
Roman Bogdanovskiy <romain.bogdanovsky@yandex.ru>
2024-03-25 23:28:05 +0300

    build(pros-const build attributes): update build text color of pros-cons block

--- build/blocks/pros-cons/index.asset.php
+++ build/blocks/pros-cons/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '7229cc9893cd05734cb9');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '8c930076d01c3a91a1bc');
