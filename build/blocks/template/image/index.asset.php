28be606d16ce61deff45ae4211fa15e32d1619a9
Roman Bogdanovskiy <romain.bogdanovsky@yandex.ru>
2024-03-26 19:12:53 +0300

    fix(template image block): fix block name for images blocks in template parent block

--- build/blocks/template/image/index.asset.php
+++ build/blocks/template/image/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => 'a2d34a23e7b262a52812');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '55aefb82b7eeba88b464');
