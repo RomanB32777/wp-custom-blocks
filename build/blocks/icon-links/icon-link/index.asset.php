dd996ffd2013de334800f91e5d8654043dc6817d
Roman Bogdanovskiy <romain.bogdanovsky@yandex.ru>
2024-04-24 23:22:14 +0300

    build(build folder): update build after fix of icon link block

--- build/blocks/icon-links/icon-link/index.asset.php
+++ build/blocks/icon-links/icon-link/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => 'a6d60e1f0e0ffce43a57');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '7300961c1291b86370b9');
