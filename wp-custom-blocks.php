5d13cff9e0e727e9e580afb0a511a5c6cc76bc33
Roman Bogdanovskiy <romain.bogdanovsky@yandex.ru>
2025-04-20 19:49:13 +0300

    0.2.10

--- wp-custom-blocks.php
+++ wp-custom-blocks.php
@@ -3,7 +3,7 @@
  * Plugin Name:       WordPress custom blocks
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           0.2.9
+ * Version:           0.2.10
  * Author:            Test
  * License:
  * License URI:
